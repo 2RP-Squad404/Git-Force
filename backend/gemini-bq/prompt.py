72d47305e3197d3f38ef2dac2bdb28c620259951
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-10-07 14:04:13 -0300

    feat: code optimizations

--- backend/gemini-bq/prompt.py
+++ backend/gemini-bq/prompt.py
@@ -1,17 +1,17 @@
 from gemini import generate
-from bigquery import create_view, get_data, get_schema, create_sqlx, string_cleaned
+from bigquery import create_view, get_data, get_schema, create_sqlx, string_cleaned, name_view_sqlx
 from dataformTest import upload_sqlx_file, execute_job
 
 # Obter as variáveis necessárias
 project_id = "tarefa-squad"
 repository_id = "teste" # Nome simples do repositório
 workspace = "testeDataform" # A branch onde você quer fazer o upload
-file_path_local = "cartao_teste.sqlx" # Caminho do arquivo local
-file_path_repo = "eventos_view.sqlx" # Nome do arquivo dentro do repositório
 dataset = input('Em qual dataset está sua tabela?\n')
 table = input('Em qual tabela quer consultar?\n')
 target_dataset = input('Em qual dataset será salvo a view?\n')
 name_view = input('Qual o nome terá a view?\n')
+file_path_repo = name_view_sqlx(name_view) # Nome do arquivo dentro do repositório
+file_path_local = "teste.sqlx" # Caminho do arquivo local
 
 schema = get_schema(dataset, table) # Obter o esquema da tabela
 
@@ -27,12 +27,12 @@ response_view = string_cleaned(response) # Limpando a consulta
 
 view = create_view(response_view, target_dataset, name_view) # Criando o script SQLX
 
-create_sqlx(view, "cartao_teste") # Criando o arquivo SQLX
+create_sqlx(view, "teste") # Criando o arquivo SQLX
 
 instruction_insight = """Você é um analista de dados especializado. Forneça insights com base nesses dados que serão apresentados. (Seja breve e objetivo)"""
 last_response = generate(data, instruction_insight) # Gerando o insight com o dado
 
 upload_sqlx_file(project_id, repository_id, workspace, file_path_local, file_path_repo)
-execute_job(project_id, repository_id, workspace, "dataform", "eventos_view")
+execute_job(project_id, repository_id, workspace, "dataform", name_view)
 
 print(last_response) # Visualizar o insight
